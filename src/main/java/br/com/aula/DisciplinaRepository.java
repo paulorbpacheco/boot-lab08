76c20e4194f47f922e2ab7afe7e87dbcef2b1c99
Paulo Pacheco <paulorb.pacheco@gmail.com>
2017-06-12 20:06:46 -0300

    Utilize o projeto definido anteriormente Defina uma nova consulta para retornar as disciplinas com a data de início maior que a data atual Configure a anotação @RestResource para expor e customizar a publicação do REST endpoint de consulta Execute e teste a aplicação

--- src/main/java/br/com/aula/DisciplinaRepository.java
+++ src/main/java/br/com/aula/DisciplinaRepository.java
@@ -2,7 +2,15 @@ package br.com.aula;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Date;
+import java.util.List;
 
 @RepositoryRestResource
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>{
+interface DisciplinaRepository extends JpaRepository<Disciplina, Long>{
+
+    @RestResource(path="dataInicioMaior")
+    List<Disciplina> findByDataInicioAfter(Date dataAtual);
 }
+
